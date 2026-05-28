d8e622487cac52d2e16c507981901981e1ac9a7d
ltgiang2 <letruonggiang7gpl@gmail.com>
2026-05-28 11:00:54 +0700

    fix: export batchCreatePoolConnections from models/index.js

--- src/models/index.js
+++ src/models/index.js
@@ -4,6 +4,7 @@ export {
   getProviderConnectionById,
   createProviderConnection,
   updateProviderConnection,
+  batchCreatePoolConnections,
   deleteProviderConnection,
   getProviderNodes,
   getProviderNodeById,
